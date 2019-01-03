55cf76353caa1663806d4fcce760f3d90e029e48
Justin Lafferty <lafferjm@gmail.com>
2019-01-02 19:27:55 -0600

    routing start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Route, Link } from 'react-router-dom'
 
 import Heroes from './components/heroes';
 
 const App = () => (
   <>
     <h1>Tour of Heroes</h1>
-    <Heroes />
+    <nav>
+      <Link to="/heroes">Heroes</Link>
+    </nav>
+    <Route path="/heroes" component={Heroes} />
   </>
 );
 
